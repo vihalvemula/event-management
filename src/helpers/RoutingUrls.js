d72432a9b927b1c4ef73fbad7d6a2ba4acc40715
vihal vemula <vihalrao704@gmail.com>
2024-05-11 19:04:22 -0500

    Integrated login page

---
+++ src/helpers/RoutingUrls.js
@@ -0,0 +1,10 @@
+const go = {
+  login: "/login",
+  adminLogin: "/admin-login",
+  adminCreateAcc: "/admin-create-account",
+  forgotPwd: "/reset-password",
+  createAcc: "/create-account",
+  homePage: "/homepage",
+};
+
+export default go;
